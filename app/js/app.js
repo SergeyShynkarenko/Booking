82b44965c4d96e9dcb0596cc93aa7587ea56dafd
SergeyShynkarenko <serg.shunkarenko@gmail.com>
2021-01-25 22:11:09 +0300

    testimonials markup

--- app/js/app.js
+++ app/js/app.js
@@ -7,5 +7,13 @@ $(function() {
 		$('.hamburger').removeClass('is-active');
 		$('.menu__list').removeClass('menu__list--active');
 	});
+
+	$('.testimonials__slider').slick({
+		slidesToShow: 1,
+	  slidesToScroll: 1,
+		prevArrow: '<button type="button" class="slick-prev"><img src="images/dest/Arrow-left.svg" alt=""></button>',
+		nextArrow: '<button type="button" class="slick-next"><img src="images/dest/Arrow-right.svg" alt=""></button>'
+	});
+
 });
 
