66bf87e2000fae6b1b139da651d523d3621b8500
SergeyShynkarenko <62879633+SergeyShynkarenko@users.noreply.github.com>
2021-01-27 18:09:48 +0300

    Testimonials (#4)

--- app/js/app.js
+++ app/js/app.js
@@ -7,5 +7,23 @@ $(function() {
 		$('.hamburger').removeClass('is-active');
 		$('.menu__list').removeClass('menu__list--active');
 	});
+
+	$('.testimonials__slider').slick({
+		speed: 300,
+		slidesToShow: 1,
+	  slidesToScroll: 1,
+		prevArrow: '<button type="button" class="slick-prev"><img src="images/dest/Arrow-left.svg" alt=""></button>',
+		nextArrow: '<button type="button" class="slick-next"><img src="images/dest/Arrow-right.svg" alt=""></button>',
+		responsive: [
+    	{
+      	breakpoint: 480,
+      	settings: {
+        	arrows: false,
+        	dots: true,
+      	}
+    	}
+  	]
+	});
+
 });
 
