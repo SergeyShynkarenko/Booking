5fb20db0cc62474897c64d762d7493523a0cc605
SergeyShynkarenko <serg.shunkarenko@gmail.com>
2021-01-27 17:17:58 +0300

    fix input slider

--- app/js/app.js
+++ app/js/app.js
@@ -9,10 +9,20 @@ $(function() {
 	});
 
 	$('.testimonials__slider').slick({
+		speed: 300,
 		slidesToShow: 1,
 	  slidesToScroll: 1,
 		prevArrow: '<button type="button" class="slick-prev"><img src="images/dest/Arrow-left.svg" alt=""></button>',
-		nextArrow: '<button type="button" class="slick-next"><img src="images/dest/Arrow-right.svg" alt=""></button>'
+		nextArrow: '<button type="button" class="slick-next"><img src="images/dest/Arrow-right.svg" alt=""></button>',
+		responsive: [
+    	{
+      	breakpoint: 480,
+      	settings: {
+        	arrows: false,
+        	dots: true,
+      	}
+    	}
+  	]
 	});
 
 });
